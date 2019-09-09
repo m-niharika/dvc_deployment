4498672328da900e9659f6b30ed8214a2fe909a2
m-niharika <54393285+m-niharika@users.noreply.github.com>
2019-09-09 19:18:16 +0530

    unpickle

--- code/app.py
+++ code/app.py
@@ -17,7 +17,7 @@ port = int(os.getenv("PORT", 9999))
 model_file = conf.model
 
 with open(model_file, 'rb') as f:
-    u = pickle._Unpickler(f)
+    u = pickle.Unpickler(f)
     u.encoding = 'latin1'
     model = u.load()
  
